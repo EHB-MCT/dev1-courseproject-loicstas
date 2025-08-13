c4aae7e6d1677db142b63b5953a5cbe5cf453dec
Loic Stas <loic.stas@student.ehb.be>
2025-08-13 19:37:32 +0200

    tweede zit

--- TweedeZit/coursproject2.js
+++ TweedeZit/coursproject2.js
@@ -9,8 +9,6 @@ let height = context.canvas.height;
 
 let bubbles = [];
 
-
-
 setup();
 update();
 
@@ -74,31 +72,74 @@ function update() {
     context.fillRect(0, 0, width, height);
 
     drawbubble();
+    createLineCircleText();
     
 
     requestAnimationFrame(update);
 }
 
 function drawbubble() {
+     const border = borderRectangles[0];
 
     for (let i = 0; i < bubbles.length; i++) {
         let bubble = bubbles[i];
 
-        context.fillStyle = 'red';
+        context.fillStyle = 'green';
         Utils.fillCircle(bubble.x, bubble.y, bubble.size); 
         
         bubble.x += bubble.hSpeed;
         bubble.y += bubble.vSpeed;
 
-             
-        if (bubble.x + bubble.size >= width || bubble.x - bubble.size <= 0) {
+        if (bubble.x + bubble.size >= border.rectX + border.rectWidth || bubble.x - bubble.size 
+            <= border.rectX) {
+
             bubble.hSpeed *= -1;
         }
         
-     
-        if (bubble.y + bubble.size >= height || bubble.y - bubble.size <= 0) {
+        if (bubble.y + bubble.size >= border.rectY + border.rectHeight || bubble.y - bubble.size 
+            <= border.rectY) {
+
             bubble.vSpeed *= -1;
+        }
+
+        for (let j = 0; j < rectangles.length; j++) {
+            let rect = rectangles[j];
+            if (bubble.x + bubble.size > rect.rectX &&
+                bubble.x - bubble.size < rect.rectX + rect.rectWidth &&
+                bubble.y + bubble.size > rect.rectY &&
+                bubble.y - bubble.size < rect.rectY + rect.rectHeight) {
+                
+               bubble.hSpeed *= -1;
+            }
         }
     }
 }
 
+function createLineCircleText(){ 
+    const border = borderRectangles[0];
+
+    context.strokeStyle = "darkgreen";
+
+    context.beginPath();
+    context.moveTo(width / 2, border.rectY); 
+    context.lineTo(width / 2, border.rectY + border.rectHeight);
+    context.stroke();
+
+    const smallestDimension = Math.min(border.rectWidth, border.rectHeight);
+    const radius = smallestDimension * 0.20; 
+
+    context.beginPath();
+    context.arc(width / 2, height / 2, radius, 0, Math.PI * 2);
+    context.stroke();
+
+    const fontSize = radius * 0.5; 
+
+    context.fillStyle = "darkgreen";
+    context.font = "bold " + fontSize + "px Arial"; 
+
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+
+    context.fillText("loïc stas", width / 2, height / 2);
+
+}
